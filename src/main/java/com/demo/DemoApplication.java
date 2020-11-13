53de0aef7a29d2b50efa65ccea4ae9ae06ce6943
liuxinjialol <13811257080@163.com>
2020-11-13 13:15:37 +0800

    Update DemoApplication.java

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-//@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
 
@@ -15,17 +18,31 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
     
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
     @RestController
     public class TestController {
+    	
+    	
+    	private final RestTemplate restTemplate;
+
+        @Autowired
+        public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+    	
 
         @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
         public String echo(@PathVariable String str) {
             return str+",world";
         }
         
-        @RequestMapping(value = "/", method = RequestMethod.GET)
-        public String root() {
-            return "this is root";
+        
+       
+        @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET)
+        public String hello(@PathVariable String id) {
+        	return "hello,"+restTemplate.getForObject("http://user/get/" + id, String.class);
         }
         
         
